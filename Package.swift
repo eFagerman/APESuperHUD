8535ef7a5f28d52ad54531686cdf74ccf5e274ce
eFagerman <e.fagerman@gmail.com>
2021-03-06 12:19:00 +0100

    Update targets in the package file

--- Package.swift
+++ Package.swift
@@ -22,14 +22,16 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
         .binaryTarget(
             name: "APESuperHUD",
-            path: "./Sources/APESuperHUD.xcframework")
+            path: "./Sources/APESuperHUD.xcframework"),
         
         .target(
             name: "APESuperHUD",
             dependencies: [],
             path: "Source"),
+        
         .testTarget(
             name: "APESuperHUDTests",
             dependencies: ["APESuperHUD"],
